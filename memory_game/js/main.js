6340bcc5b9652421c95fbc6108b8eb9ed3c9c3f5
Louis Ballard <louishballard@me.com>
2020-03-31 21:11:06 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,28 @@
-console.log("Up and running!");
+let cards = ['queen', 'queen', 'king', 'king']
+let cardsInPlay = [];
 
 var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log ("User flipped " + cardOne);
-console.log ("User flipped " + cardThree);
+console.log("User flipped " + cardOne);
+cardsInPlay.push('cardOne');
+
+var cardTwo = "king";
+console.log("User flipped " + cardTwo);
+cardsInPlay.push('cardTwo');
+
+if(cardsInPlay.length === 2) {
+
+}
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
+
+
+
+
+
+
+
+
